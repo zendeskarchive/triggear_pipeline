f76d4b0f26de08ef2495cfd1eb5f1d1ce5075531
karolgil <karol.gil@getbase.com>
2017-11-30 16:01:04 +0100

    GitHubRepostiory as interface to avoid unclassified new exception on GitHub

--- triggear/GitHubRepository.groovy
+++ triggear/GitHubRepository.groovy
@@ -1,22 +1,8 @@
 package com.futuresimple.triggear
 
-class GitHubRepository implements Serializable {
-    String organizationName
-    String repositoryUrl
-    String repositoryName
-    String repositoryFullName
-
-    protected GitHubRepository(){}
-    
-    /**
-     * Creates a simple 'DTO' which carries couple of forms of GitHub repo URL and names.
-     *
-     * @param repositoryName Short repository name, e.g. for git@github.com:futuresimple/triggear pass here 'futuresimple' and 'triggear'
-     */
-    GitHubRepository(String organizationName, String repositoryName){
-        this.organizationName = organizationName
-        this.repositoryUrl = "git@github.com:${organizationName}/${repositoryName}.git"
-        this.repositoryName = repositoryName
-        this.repositoryFullName = "${organizationName}/${repositoryName}"
-    }
+interface GitHubRepository {
+    String getOrganizationName()
+    String getRepositoryUrl()
+    String getRepositoryName()
+    String getRepositoryFullName()
 }
